5e2ef2d8c6d79b4bb81c24bcd85aa8f5162d5816
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-03-22 07:08:27 +0300

    Add action for sending messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def send_message
+    receiver = User.find(params[:receiver_id])
+    sender = User.find(params[:sender_id])
+    @message = sender.sent_messages.build(receiver: receiver, content: params[:content])
+
+    if @message.save
+      # Message created successfully
+      render json: @message, status: :created
+    else
+      # Handle validation errors or other failures
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
 end
