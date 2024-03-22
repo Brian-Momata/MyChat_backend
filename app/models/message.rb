c8ce40cd9c2f2a9d3ee1c925c88efb4b26922540
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-03-22 07:51:21 +0300

    Add scope to get messages between 2 users

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
   belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
+
+  scope :between_users, ->(user1_id, user2_id) {
+    where('(sender_id = ? AND receiver_id = ?) OR (sender_id = ? AND receiver_id = ?)',
+          user1_id, user2_id, user2_id, user1_id)
+  }
 end
