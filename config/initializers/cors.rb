f5c96a2a2e2639e8ec8b96838307e67170ec1c68
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-05-22 07:33:14 +0300

    Try fixing an issue with CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://momatachat.netlify.app/"
+    origins "*"
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true,
+      max_age: 86400
   end
 end
