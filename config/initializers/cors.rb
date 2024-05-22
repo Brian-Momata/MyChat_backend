d6f3ec9462e76b78762dcaccb7c93d56fd59c4a4
Brian-Momata <107246922+Brian-Momata@users.noreply.github.com>
2024-05-22 07:37:53 +0300

    Add origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://momatachat.netlify.app/"
+    origins "https://momatachat.netlify.app"
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
